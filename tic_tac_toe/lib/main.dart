5ae2344354cc0978d2500090b761aa8af4c0d8be
Kurt Sanchez <kurt.sanchez.20@usjr.edu.ph>
2024-07-04 13:09:42 +0800

    Firebase connected and template

---
+++ tic_tac_toe/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/firebase_options.dart';
+import 'package:tic_tac_toe/src/controllers/auth_controller.dart';
+import 'package:tic_tac_toe/src/routing/router.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  WidgetsFlutterBinding.ensureInitialized();
+  AuthController.initialize();
+  GlobalRouter.initialize();
+  await AuthController.I.loadSession();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: GlobalRouter.I.router,
+      title: 'Stateful Changes Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
---
+++ tic_tac_toe/lib/src/screens/home/home.screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:state_change_demo/src/controllers/auth_controller.dart';
+import 'package:state_change_demo/src/dialogs/waiting_dialog.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String route = '/home';
+  static const String path = "/home";
+  static const String name = "Home Screen";
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          height: 52,
+          child: ElevatedButton(
+            onPressed: () {
+              WaitingDialog.show(context, future: AuthController.I.logout());
+            },
+            child: const Text("Logout"),
+          ),
+        ),
+      ),
+      body: const SafeArea(
+          child: Center(
+        child: Text("Home"),
+      )),
+    );
+  }
+}

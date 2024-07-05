eff7613d517e7eb8ceaa1bfda37013941da60828
Rhunnan <143944172+Rhunnan@users.noreply.github.com>
2024-07-06 02:02:19 +0800

    added gameScreen placeholder (#1)

--- tic_tac_toe/lib/src/screens/home/wrapper.dart
+++ tic_tac_toe/lib/src/screens/home/wrapper.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:tic_tac_toe/src/screens/game/game_screen.dart';
 import 'package:tic_tac_toe/src/screens/home/home.screen.dart';
 
 import '../../routing/router.dart';
@@ -15,7 +16,7 @@ class HomeWrapper extends StatefulWidget {
 class _HomeWrapperState extends State<HomeWrapper> {
   int index = 0;
 
-  List<String> routes = [HomeScreen.route, "/index"];
+  List<String> routes = [HomeScreen.route, GameScreen.route];
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +36,7 @@ class _HomeWrapperState extends State<HomeWrapper> {
         },
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.menu), label: "Index"),
+          BottomNavigationBarItem(icon: Icon(Icons.games), label: "Game"),
         ],
       ),
     );

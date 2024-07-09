05b783c0243c3e7dcd5cb927c3d2f6e5b10883f0
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2024-07-09 16:54:16 +0800

    added message Model

---
+++ tic_tac_toe/lib/src/models/message_model.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Message {
+  final String id;
+  final String senderId;
+  final String content;
+  final DateTime createdAt; // Assuming this is a timestamp field from Firestore
+
+  Message({
+    required this.id,
+    required this.senderId,
+    required this.content,
+    required this.createdAt,
+  });
+
+  // Factory constructor to easily create a Message from a Map
+  factory Message.fromJson(Map<String, dynamic> json) {
+    return Message(
+      id: json['id'],
+      senderId: json['senderId'],
+      content: json['content'],
+      createdAt: (json['createdAt'] as Timestamp)
+          .toDate(), // Convert Timestamp to DateTime
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'senderId': senderId,
+      'content': content,
+      'createdAt':
+          Timestamp.fromDate(createdAt), // Convert DateTime to Timestamp
+    };
+  }
+}

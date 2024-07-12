c29a39697937efe70ec989ac46394e75585bd909
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2024-07-12 23:22:29 +0800

    remvoe unused import

--- tic_tac_toe/lib/src/screens/game/game_screen.dart
+++ tic_tac_toe/lib/src/screens/game/game_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:js_interop';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:tic_tac_toe/src/controllers/auth_controller.dart';
